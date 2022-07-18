b0eee782d188c8106d37682cd3c39191be737dbc
Martin Frederic <juni@fat.fm>
2022-07-19 01:17:35 +0200

    Make Scrobbler.clear_staged_track() public

--- Scrobbler.h
+++ Scrobbler.h
@@ -11,11 +11,11 @@ class Scrobbler
 public:
   void stage_track(std::wstring artist, std::wstring title);
   void commit_track();
+  void clear_staged_track();
 
 private:
   Track* staged_track = nullptr;
   std::chrono::steady_clock::time_point staged_time;
   std::list<Track*> submissions;
-  void clear_staged_track();
 };
 
