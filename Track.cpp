b0c52b6e4dd24b9213ac2a3b8ff902355f9314f8
Martin Frederic <juni@fat.fm>
2022-07-19 01:34:45 +0200

    Remember a track's length

--- Track.cpp
+++ Track.cpp
@@ -6,7 +6,8 @@
 /* How can a language be this fucking ugly... */
 Track::Track(std::wstring artist, std::wstring title) :
   artist(artist),
-  title(title)
+  title(title),
+  length(INVALID_TRACK_LENGTH)
   {}
 
 std::basic_ostream<wchar_t>& operator<<(std::basic_ostream<wchar_t>& stream, const Track& track) {
--- Track.h
+++ Track.h
@@ -2,12 +2,15 @@
 
 #include <string>
 
+#define INVALID_TRACK_LENGTH -1
+
 class Track
 {
 public:
   Track(std::wstring artist, std::wstring title);
   std::wstring artist;
   std::wstring title;
+  int length; // seconds
 };
 
 std::basic_ostream<wchar_t>& operator<<(std::basic_ostream<wchar_t>& stream, const Track& track);
